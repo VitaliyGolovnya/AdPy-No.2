88bf11e3311379f71bba2646cf267ea60ee78925
Golovnya Vitaliy <vitas210295@gmail.com>
2022-03-13 10:37:39 +1000

    removed repeated contacts

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import csv
 with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
+
 phone_pattern = re.compile(r'(8|\+7)[\s|\-]*\(?(\d{3})\)?[\s|\-]*(\d{3})[\s|\-]*(\d{2})[\s|\-]*(\d{2})(\s)?\(?(доб\.)?\s*(\d+)?\)?')
 name_pattern1 = re.compile(r'(^\w+)\s+(\w+)\s+(\w+)(,{3})(.*)')
 name_pattern2 = re.compile(r'(^\w+)\s+(\w+)(,{2})(.*)')
@@ -16,8 +17,16 @@ for id, contact in enumerate(contacts_list):
     string = re.sub(name_pattern2, r'\1,\2,\4', string)
     contacts_list[id] = string.split(',')
 
-
-
+contact_id = {}
+for id, contact in enumerate(contacts_list):
+    full_name = contact[0] + contact[1]
+    if full_name not in contact_id.keys():
+        contact_id[full_name] = id
+    else:
+        for index, original_value in enumerate(contacts_list[contact_id[full_name]]):
+            if original_value == "":
+                contacts_list[contact_id[full_name]][index] = contact[index]
+        contacts_list.pop(id)
 
 with open("phonebook.csv", "w", encoding='utf-8') as f:
   datawriter = csv.writer(f, delimiter=',')
